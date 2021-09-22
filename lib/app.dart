e61e22b7481b433941b3395e439f3ba6a99df2b3
amarildolacerAda <amarildo_51@msn.com>
2021-09-22 13:16:39 -0300

    =

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:logistic/views/home/home_view.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
+import 'controller/config/settings_controller.dart';
+import 'controller/config/settings_view.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -26,6 +25,7 @@ class MyApp extends StatelessWidget {
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           // Providing a restorationScopeId allows the Navigator built by the
           // MaterialApp to restore the navigation stack when a user leaves and
           // returns to the app after it has been killed while running in the
@@ -42,7 +42,8 @@ class MyApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''), // English, no country code
+            Locale('en', ''),
+            Locale('pt_BR', ''), // English, no country code
           ],
 
           // Use AppLocalizations to configure the correct application title
@@ -69,11 +70,11 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                  //case SampleItemDetailsView.routeName:
+                  //   return const SampleItemDetailsView();
+                  // case SampleItemListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const HomeView();
                 }
               },
             );
---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:
+            AppBar(elevation: 0, title: const Text('Storeware - Logistica')),
+        body: Container());
+  }
+}
